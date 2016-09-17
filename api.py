f08129597ad02fb0d15ac301be3f51475d4d3864
Felix Morgner <felix.morgner@gmail.com>
2016-09-17 14:35:08 +0200

    Add missing resources

--- api.py
+++ api.py
@@ -1,9 +1,48 @@
-from tastypie import resources
+from tastypie.resources import NamespacedModelResource
+from tastypie.fields import ToManyField, ForeignKey
 
 from clubm8core import models
 
 
-class EventResource(resources.ModelResource):
+class TagResource(NamespacedModelResource):
+    class Meta:
+        queryset = models.Tag.objects.all()
+        resource_name = 'tag'
+
+
+class EventResource(NamespacedModelResource):
+    tags = ToManyField(TagResource, 'tag')
+
     class Meta:
         queryset = models.Event.objects.all()
         resource_name = 'event'
+
+
+class OccurenceResource(NamespacedModelResource):
+    event = ForeignKey(EventResource, 'event')
+
+    class Meta:
+        queryset = models.Occurrence.objects.all()
+        resource_name = 'occurence'
+
+
+class SpecialOccurenceResource(NamespacedModelResource):
+    event = ForeignKey(EventResource, 'event')
+
+    class Meta:
+        queryset = models.SpecialOccurrence.objects.all()
+        resource_name = 'special'
+
+
+class PlanResource(NamespacedModelResource):
+    occurrences = ToManyField(OccurenceResource, 'occurence')
+
+    class Meta:
+        queryset = models.Plan.objects.all()
+        resources = 'plan'
+
+
+class SlotResource(NamespacedModelResource):
+    class Meta:
+        slot = models.Slot.objects.all()
+        resource_name = 'slot'
---
+++ urls.py
@@ -0,0 +1,20 @@
+from tastypie.api import NamespacedApi
+from clubm8api.api import (EventResource,
+                           PlanResource,
+                           OccurenceResource,
+                           SlotResource,
+                           SpecialOccurenceResource,
+                           TagResource,
+                           )
+
+app_name = 'clubm8api'
+
+api = NamespacedApi(api_name='v1', urlconf_namespace=app_name)
+api.register(EventResource())
+api.register(PlanResource())
+api.register(OccurenceResource())
+api.register(SlotResource())
+api.register(SpecialOccurenceResource())
+api.register(TagResource())
+
+urlpatterns = api.urls
